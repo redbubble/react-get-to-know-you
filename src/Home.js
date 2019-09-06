8d502abf7e3c7aec6ccd40319b6e315494e20af4
Marcus Hammarstedt <marcus.hammarstedt@redbubble.com>
2019-09-06 17:24:42 +0200

    Adding styling and update content

--- src/Home.js
+++ src/Home.js
@@ -33,45 +33,44 @@ const Speech = ({ children, reverse, comment, character }) => (
     <div className={styles.speechBody}>
       <article className={styles.speechBubble}>
         <div className={styles.speechHeader}>
-          <span className={styles.speechNameMobile}>
-            <strong>{ character.name }</strong> - { character.role }
-          </span>
+          <div role="img" aria-label={`Avatar for: ${character.name}`} className={styles.speechAvatar} style={{ backgroundImage: `url(${character.img})` }}/>
+          <div className={styles.speechUser}>
+            <div className={styles.speecUserName}><strong>{ character.name }</strong></div>
+            <div className={styles.speechUserRole}>{ character.role }</div>
+          </div>
         </div>
         <div className={styles.speechContent}>
           { children }
         </div>
       </article>
-      <div className={styles.speechName}>
-        <strong>{ character.name }</strong> - { character.role }
-      </div>
     </div>
   </div>
 );
 
 const Home = () => (
   <React.Fragment>
-    <section className={styles.section}>
-      <h1>Redbubble Design System - Frontend Engineer</h1>
-      <p className={styles.text}>
-        Welcome to the "Get to know you" exercise for the role of <strong>Frontend Engineer</strong> in Redbubble's <strong>Design System</strong> team.
-      </p>
-
-      <p className={styles.text}>
-        Below, you'll find a typical conversation that our Design System team would have when a new component is proposed.
-      </p>
+    <section className={styles.section} style={{ marginTop: '48px' }}>
+      <div style={{ maxWidth: '640px' }}>
+        <h1>Hi there <span role="img" aria-label="Waving Hand Icon">👋</span></h1>
+        <p className={styles.text}>
+          Welcome, to the "Get to know you" exercise for the role of <strong>Frontend Engineer</strong> in Redbubble's <strong>Design System</strong> team. We're excited and thankful that you're here!
+        </p>
+      </div>
     </section>
-
     <section className={styles.section}>
-      <h2>
-        <span role="img" aria-label="Speech Bubble Icon">💬</span> Background Context
-      </h2>
-
-      <p className={styles.text}>
-        Have a read to catch up on the context:
-      </p>
-
+      <div style={{ maxWidth: '640px' }}>
+        <h2>Intro</h2>
+        <p className={styles.text}>
+          Below, you'll find a typical conversation that our Design System team would have when a new component is proposed.
+        </p>
+        <p className={styles.text}>
+          You will be asked to participate in the conversation. <strong>So have a read to catch up on the context.</strong>
+        </p>
+      </div>
+    </section>
+    <section>
       <div className={styles.conversation}>
-        <Speech character={katherine}>
+        <Speech reverse character={katherine}>
           <h1>Summary</h1>
 
           <p>
@@ -128,7 +127,7 @@ const Home = () => (
           </div>
         </Speech>
 
-        <Speech reverse character={eric} comment>
+        <Speech character={eric} comment>
           <p>
             Looks good Kat!
           </p>
@@ -142,7 +141,7 @@ const Home = () => (
           </p>
         </Speech>
 
-        <Speech character={jessica} comment>
+        <Speech reverse character={jessica} comment>
           <p>
             I like the extra action at the bottom. Will all tabs have one and I guess they'll be different for each tab?
           </p>
@@ -156,7 +155,7 @@ const Home = () => (
           </p>
         </Speech>
 
-        <Speech reverse character={serjio}>
+        <Speech character={serjio}>
           <p>
             I'm interested in what happens if we have too many tabs to fit across? Or is that just a guideline that we put in the documentation?
           </p>
@@ -166,122 +165,129 @@ const Home = () => (
           </p>
         </Speech>
 
-        <Speech character={you}>
+        <Speech reverse character={you}>
           ...
         </Speech>
       </div>
     </section>
 
     <section className={styles.section}>
-      <h2>
-        <span role="img" aria-label="Laptop Icon">💻</span> Next steps...
-      </h2>
+      <div style={{ maxWidth: '640px' }}>
+        <div className={styles.callout} style={{ margin: '48px 0 48px' }}>
+          <p className={styles.text}>
+            <strong>Note: </strong>
+            For the sake of this exercise, it is ok to make assumptions about any questions you might have to make sure that you can proceed.
+          </p>
+        </div>
+        <h2>
+          <span role="img" aria-label="Laptop Icon">💻</span> Next steps...
+        </h2>
 
-      <p className={styles.text}>
-        <strong>Note: </strong>
-        <em>For the sake of this exercise, it is ok to make assumptions about any questions you might have to make sure that you can proceed.</em>
-      </p>
+        <h3 className={styles.text}>
+          <span className={styles.subHeading}>Step 1</span>
+          Join in the discussion
+        </h3>
 
-      <br />
+        <p className={styles.text}>
+          Given the above context, write a response as if you were participating in the discussion.
+        </p>
 
-      <p className={styles.text}>
-        <strong>Step 1: Join in the discussion</strong>
-      </p>
+        <p className={styles.text}>
+          Share your own thoughts and opinions on what would make this a high quality, re-usable Design System component.
+        </p>
 
-      <p className={styles.text}>
-        Given the above context, write a response as if you were participating in the discussion.
-      </p>
+        <p className={styles.text}>
+          If you have any questions, about the component features or design - include them in your response.
+        </p>
 
-      <p className={styles.text}>
-        Share your own thoughts and opinions on what would make this a high quality, re-usable Design System component.
-      </p>
+        <p className={styles.text}>
+          Just a few sentences is enough :)
+        </p>
 
-      <p className={styles.text}>
-        If you have any questions, about the component features or design - include them in your response.
-      </p>
+        <br />
 
-      <p className={styles.text}>
-        Just a few sentences is enough :)
-      </p>
+        <h3 className={styles.text}>
+          <span className={styles.subHeading}>Step 2</span>
+          Draft the component
+        </h3>
 
-      <br />
+        <p className={styles.text}>
+          Now that you've chimed in, have a go at <strong>coding</strong> the described component.
+        </p>
 
-      <p className={styles.text}>
-        <strong>Step 2: Draft the component</strong>
-      </p>
+        <p className={styles.text}>
+          Some tips:
+        </p>
 
-      <p className={styles.text}>
-        Now that you've chimed in, have a go at <strong>coding</strong> the described component.
-      </p>
+        <ul className={styles.list}>
+          <li>Don't spend more than 2 hours on this exercise, we know your time is valuable <span aria-hidden="true">♥️</span></li>
+          <li>Your code <strong>does not need to be complete</strong> or production ready, just a good start that demonstrates your thinking and coding.</li>
+          <li>You don't have all the answers, so make some assumptions, try some things out - it's ok to make mistakes.</li>
+          <li>Don't sweat the visual details too much.</li>
+          <li>Code the way you like to code</li>
+          <li>We'll pair on your code later on in the interview process</li>
+        </ul>
 
-      <p className={styles.text}>
-        Some tips:
-      </p>
-
-      <ul className={styles.list}>
-        <li>Don't spend more than 2 hours on this exercise, we know your time is valuable <span aria-hidden="true">♥️</span></li>
-        <li>Your code <strong>does not need to be complete</strong> or production ready, just a good start that demonstrates your thinking and coding.</li>
-        <li>You don't have all the answers, so make some assumptions, try some things out - it's ok to make mistakes.</li>
-        <li>Don't sweat the visual details too much.</li>
-        <li>Code the way you like to code</li>
-        <li>We'll pair on your code later on in the interview process</li>
-      </ul>
-
-      <p className={styles.text}>
-        You can find setup instructions and contribution guidelines for the project{' '}
-        <a
-          target="blank"
-          rel="noopener noreferrer"
-          aria-label="Go the the project setup instructions on GitHub.com"
-          href="https://github.com/redbubble/react-get-to-know-you"
-        >
-          here
-        </a>
-      </p>
+        <p className={styles.text}>
+          You can find setup instructions and contribution guidelines for the project{' '}
+          <a
+            target="blank"
+            rel="noopener noreferrer"
+            aria-label="Go the the project setup instructions on GitHub.com"
+            href="https://github.com/redbubble/react-get-to-know-you"
+          >
+            here
+          </a>
+        </p>
+      </div>
     </section>
 
     <section className={styles.section}>
-      <h2>
-        <span role="img" aria-label="Postbox Icon">📮</span> When you're done
-      </h2>
+      <div style={{ maxWidth: '640px' }}>
+        <h2>
+          <span role="img" aria-label="Postbox Icon">📮</span> When you're done
+        </h2>
 
-      <p className={styles.text}>
-        When you're ready, you can submit your code and response to us via the link in the same email where you found these instructions.
-      </p>
+        <p className={styles.text}>
+          When you're ready, you can submit your code and response to us via the link in the same email where you found these instructions.
+        </p>
 
-      <p className={styles.text}>
-        <strong><span role="img" aria-label="Face with finger saying sshh icon">🤫</span> Anonymity</strong>
-      </p>
+        <p className={styles.text}>
+          <strong><span role="img" aria-label="Face with finger saying sshh icon">🤫</span> Anonymity</strong>
+        </p>
 
-      <p className={styles.text}>
-        We'll evaluate you most fairly if only our talent team knows whose submission is being evaluated.
-      </p>
+        <p className={styles.text}>
+          We'll evaluate you most fairly if only our talent team knows whose submission is being evaluated.
+        </p>
 
-      <p className={styles.text}>
-        Please don't put your name or other identifying information in your submission.
-      </p>
+        <p className={styles.text}>
+          Please don't put your name or other identifying information in your submission.
+        </p>
+      </div>
     </section>
 
     <section className={styles.section}>
-      <h2>
-        <span role="img" aria-label="Hears icon">💕</span> Have fun
-      </h2>
+      <div style={{ maxWidth: '640px' }}>
+        <h2>
+          <span role="img" aria-label="Hears icon">💕</span> Have fun
+        </h2>
 
-      <p className={styles.text}>
-        This is the most important bit.
-      </p>
+        <p className={styles.text}>
+          This is the most important bit.
+        </p>
 
-      <p className={styles.text}>
-        We love what we do and are excited to come to work each day to build great components for our users.
-      </p>
+        <p className={styles.text}>
+          We love what we do and are excited to come to work each day.
+        </p>
 
-      <p className={styles.text}>
-        We know this sort of thing can take lot of time out of your busy schedule, so we ask that you do only what you're comfortable with and nothing more.
-      </p>
+        <p className={styles.text}>
+          We know this sort of thing can take lot of time out of your busy schedule, so we ask that you do only what you're comfortable with and nothing more.
+        </p>
 
-      <p className={styles.text}>
-        We hope you have fun working on this, don't take it too seriously - mostly we just want to get an idea of how you think and approach problems.
-      </p>
+        <p className={styles.text}>
+          We hope you have fun working on this, don't take it too seriously - mostly we just want to get an idea of how you think and approach problems.
+        </p>
+      </div>
     </section>
   </React.Fragment>
 );
